f54ce461938380f40abe78bd7344551e7515c13f
Kriston Rickman <kristonrickman@gmail.com>
2026-09-05 23:10:03 -0700

    v1_05

---
+++ src/Aircraft_constraint_states.m
@@ -0,0 +1,21 @@
+%Created by Kriston Rickman
+%Date created 09/03/26
+%v1_05 for Energy based constraint analysis v1_31
+%States 
+%Notes: 
+
+
+
+classdef Aircraft_constraint_states
+    enumeration
+        MAIN_MENU
+        SAVED_TW_AIRCRAFT_DATA
+        USER_TW_AIRCRAFT_MENU
+        EDIT_SAVED_TW_AIRCRAFT
+        GET_TW_AIRCRAFT_DATA
+        CALCULATE_TW
+        GRAPH_TW
+        GRAPH_TW_FROM_USER_TW_DATA_CALCULATION
+        ERROR_STATE %need to create
+    end
+end
